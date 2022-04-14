c8fb66fc036a0b2f95c18d0366ae4053ed3309b5
Vlad Sava <savavlad82@gmail.com>
2022-04-14 12:05:17 +0300

    Added ties and announcement of winner

--- script.js
+++ script.js
@@ -10,16 +10,35 @@ const gameBoard = (() => {
         });
     });
     const checkWinner = () =>{
+        let ok = 1;
         if((board[0] === board[1] && board[1] === board[2]) || (board[0] === board[3] && board[3] === board[6]) || (board[0] === board[4] && board[4] === board[8]))
-            alert(`The winner is Player ${board[0]}`);
+            return displayWinner(board[0]);
         else if(board[1] === board[4] && board[4] === board[7])
-            alert(`The winner is Player ${board[1]}`);
+            return displayWinner(board[1]);
         else if((board[2] === board[4] && board[4] === board[6]) || (board[2] === board[5] && board[5] === board[8]))
-            alert(`The winner is Player ${board[2]}`);
+            return displayWinner(board[2]);
         else if((board[3] === board[4] && board[4] === board[5]))
-            alert(`The winner is Player ${board[3]}`);
+            return displayWinner(board[3]);
         else if((board[6] === board[7] && board[7] === board[8]))
-            alert(`The winner is Player ${board[6]}`);
+            return displayWinner(board[6]);
+        for(let i = 0; i < 9; i++){
+            if(board[i] < 0)
+                ok = 0;
+        }
+        if(ok === 1)
+            return displayWinner('tie');
+    }
+    const displayWinner = (winner) => {
+        let winnerText = document.createElement('div');
+        if(typeof winner == "number"){
+            winnerText.textContent = `The winner is Player ${winner + 1}.`;
+            winnerText.classList.add('winnerText');
+        }
+        else{
+            winnerText.textContent = `It's a tie.`;
+            winnerText.classList.add('winnerText');
+        }
+        document.body.appendChild(winnerText);
     };
     return {board, checkWinner};
 })();
